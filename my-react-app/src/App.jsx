9647885325f1ac3c7ebd78cf4505d7c9745842d8
Xclusivkhodes <xclusivkhoder@gmail.com>
2025-12-02 16:18:02 +0000

    Assets Added

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "./components/search";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <main>
       <div className="pattern">
         <div className="wrapper">
           <header>
+            <img src="../public/hero.png" alt="Hero Banner" />
             <h1>
               Find <span className="text-gradient">Movies</span> You'll Enjoy
               Without The Hustle
             </h1>
           </header>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </div>
       </div>
     </main>
