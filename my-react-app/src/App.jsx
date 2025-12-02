d13e4cd4de859ce47d4083a368502a5c23231611
Xclusivkhodes <xclusivkhoder@gmail.com>
2025-12-02 12:38:00 +0000

    First Commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+              Without The Hustle
+            </h1>
+          </header>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
